3175bfa6065ec765bcae7719e7d911e9af250bd9
Adham-hani <adham240103327@sut.edu.eg>
2025-12-03 21:53:26 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,22 +1,12 @@
-//ASCII Art Contest
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int main() {
-    int g, c, l;
-    cin >> g >> c >> l;
-
-    int max_score = max({g, c, l});
-    int min_score = min({g, c, l});
-
-    if (max_score - min_score >= 10) {
-        cout << "check again" << endl;
-    } else {
-        int scores[3] = {g, c, l};
-        sort(scores, scores + 3);
-        cout << "final " << scores[1] << endl;
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < i; j++) cout << "*";
+        cout << "\n";
     }
-
     return 0;
 }

3afa9d97d1c6ceddf44515d4f5a0f0bed1b9dd43
Adham-hani <adham240103327@sut.edu.eg>
2025-12-05 06:03:32 -0800

    Update main.cpp

--- trianngle star/main.cpp
+++ trianngle star/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < i; j++) cout << "*";
+        cout << "\n";
+    }
     return 0;
 }

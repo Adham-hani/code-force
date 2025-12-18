75f05d9fded8f473b42ac152127a34c53cc349f3
Adham-hani <adham240103327@sut.edu.eg>
2025-12-17 23:58:54 -0800

    Add files via upload

---
+++ Triangle Validity/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+CODE
+
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a + b > c && a + c > b && b + c > a) {
+        cout << "Valid";
+    } else {
+        cout << "Invalid";
+    }
+
+    return 0;
+}
+
